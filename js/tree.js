02a160350f9206fd4faee5a5dca1882058eb5a7e
Mikhail Burmistrov <schlaf2003@mail.ru>
2024-11-11 15:26:44 +0300

    Update tree.js

--- js/tree.js
+++ js/tree.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const toggler = document.getElementsByClassName("caret");
     for (let i = 0; i < toggler.length; i++) {
         toggler[i].addEventListener("click", function () {
+            // Prevent other click events from interfering
             this.parentElement.querySelector(".nested").classList.toggle("active");
             this.classList.toggle("caret-down");
         });
