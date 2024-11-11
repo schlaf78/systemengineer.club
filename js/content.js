9e3c221ccbf2ce4a4eabb57c509db41668042070
Mikhail Burmistrov <schlaf2003@mail.ru>
2024-11-12 01:13:43 +0300

    Update content.js

--- js/content.js
+++ js/content.js
@@ -2,11 +2,11 @@ function loadContent(topic) {
     const mainContent = document.getElementById("main-content");
 
     const content = {
-        "section1": "<h2>Section 1</h2><p>This is content for Section 1 kjhhjghjk.</p>",
+        "section1": "<h2>Ubuntu</h2><p>This is content for Ubuntu section</p>",
         "topic1.1": `
-            <h2>Topic 1.1</h2>
+            <h2>Create User</h2>
             <p>Ubuntu</p>
-            <img src="https://avatars.githubusercontent.com/u/27727118?v=4" alt="Description of Example Image" style="max-width: 100%; height: auto;">
+#            <img src="https://avatars.githubusercontent.com/u/27727118?v=4" alt="Description of Example Image" style="max-width: 100%; height: auto;">
         `,
         "topic1.2": "<h2>Topic 1.2</h2><p>This is the content for Topic 1.2 under Section 1 99999.</p>",
         "section2": "<h2>Section 2</h2><p>This is content for Section 2.</p>",
