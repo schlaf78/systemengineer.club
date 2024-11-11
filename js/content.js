4dc7b951db3af69ce3c32f034a9c4f5f503a9119
Mikhail Burmistrov <schlaf2003@mail.ru>
2024-11-12 01:10:44 +0300

    Update content.js

--- js/content.js
+++ js/content.js
@@ -5,7 +5,7 @@ function loadContent(topic) {
         "section1": "<h2>Section 1</h2><p>This is content for Section 1 kjhhjghjk.</p>",
         "topic1.1": `
             <h2>Topic 1.1</h2>
-            <p>This is the content for Topic 1.1 under Section 15555.</p>
+            <p>Ubuntu</p>
             <img src="https://avatars.githubusercontent.com/u/27727118?v=4" alt="Description of Example Image" style="max-width: 100%; height: auto;">
         `,
         "topic1.2": "<h2>Topic 1.2</h2><p>This is the content for Topic 1.2 under Section 1 99999.</p>",
