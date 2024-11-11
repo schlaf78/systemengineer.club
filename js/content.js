47190fb7e602420d76c247926ba61adb0b7e587b
Mikhail Burmistrov <schlaf2003@mail.ru>
2024-11-12 01:07:07 +0300

    Update content.js

--- js/content.js
+++ js/content.js
@@ -5,10 +5,10 @@ function loadContent(topic) {
         "section1": "<h2>Section 1</h2><p>This is content for Section 1 kjhhjghjk.</p>",
         "topic1.1": `
             <h2>Topic 1.1</h2>
-            <p>This is the content for Topic 1.1 under Section 1.</p>
+            <p>This is the content for Topic 1.1 under Section 15555.</p>
             <img src="https://yourusername.github.io/your-repo-name/images/example.jpg" alt="Description of Example Image" style="max-width: 100%; height: auto;">
         `,
-        "topic1.2": "<h2>Topic 1.2</h2><p>This is the content for Topic 1.2 under Section 1.</p>",
+        "topic1.2": "<h2>Topic 1.2</h2><p>This is the content for Topic 1.2 under Section 1 99999.</p>",
         "section2": "<h2>Section 2</h2><p>This is content for Section 2.</p>",
         "topic2.1": "<h2>Topic 2.1</h2><p>This is the content for Topic 2.1 under Section 2.</p>",
         "topic2.2": "<h2>Topic 2.2</h2><p>This is the content for Topic 2.2 under Section 2.</p>"
